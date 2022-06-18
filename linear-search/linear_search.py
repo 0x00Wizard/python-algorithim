19fe53b500f1674933fc89f9f94b454c1942ab91
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-18 07:48:22 -0700

    implementing linear_search.py code

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,1 +1,6 @@
-def linear_search(main_list, item)
+def linear_search(main_list, item):
+    for i in range(len(main_list)):
+        if main_list[i] == item:
+            return i
+        else:
+            return -1
