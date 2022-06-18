711fb9d80bfab7ca8d391a9653c9a3ae280a0252
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-06-18 08:05:37 -0700

    implementing linear_search.py code

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,8 +1,27 @@
+# Linear Search Algorithm
+
 def linear_search(main_list, item):
     for i in range(len(main_list)):
         if main_list[i] == item:
             return i
-        else:
-            return -1
+    return -1
 
 
+print(linear_search([1, 3, 2, 4, 5, 6, 7], 6))
+
+
+# Linear Search Algorithm with print statements)
+
+def linear_search(main_list, item):
+    print("Starting Linear Search Algorithm")
+    for i in range(len(main_list)):
+        print(f"============ Iteration #{i} ============")
+        print("Current list item:", main_list[i])
+        print("Item searched:", item)
+        print("Is the current item equal to the item searched?", main_list[i] == item)
+        if main_list[i] == item:
+            print(f"Item found! at index {i}")
+            return i
+        print("This is not the item")
+    print("The item is not in the list")
+    return -1
